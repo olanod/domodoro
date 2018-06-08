e59933f5a89474120d61587fa9ef109d2d6485eb
Daniel Olano <danielolanoc@gmail.com>
2018-06-08 18:44:10 +0200

    Create pomodoro structure with states

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-
 extern crate docopt;
 #[macro_use]
 extern crate serde_derive;
 
 use docopt::Docopt;
+use std::sync::mpsc;
+use std::thread::{spawn};
 use std::time::Duration;
-use std::thread::sleep;
 
 const USAGE: &'static str = "
 Domodoro. A pomodoro utility that can be used as a D-Bus service.
@@ -35,11 +35,75 @@ fn main() {
     let len = Duration::from_secs((args.flag_length).into());
     let brk = Duration::from_secs((args.flag_break).into());
 
-    println!("Start working on {}", args.arg_task_name);
-    loop {
-        sleep(len);
-        println!("worked for {}s, now break!", len.as_secs());
-        sleep(brk);
-        println!("rested for {}s, now continue!", brk.as_secs());
+    let (tx, rx) = mpsc::channel();
+
+    let mut pomodoro = Pomodoro::new(len, brk);
+    pomodoro.start();
+
+    spawn(move || {
+        // work a bit
+        tx.send(pomodoro.next());
+        tx.send(pomodoro.next());
+        tx.send(pomodoro.next());
+        tx.send(pomodoro.next());
+    });
+
+    for state in rx {
+        println!("Got {:?}", state);
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum State {
+    Stop,
+    Pause,
+    Work,
+    //LongBreak,
+    ShortBreak,
+}
+
+struct Pomodoro {
+    dur: Duration,
+    short_brk: Duration,
+    long_brk: Duration,
+    prev_state: State,
+    state: State,
+}
+
+impl Pomodoro {
+    fn new(dur: Duration, brk: Duration) -> Pomodoro {
+        Pomodoro {
+            dur,
+            short_brk: brk,
+            long_brk: brk * 4,
+            prev_state: State::Stop,
+            state: State::Pause,
+        }
+    }
+
+    fn start(&mut self) {
+        self.change(State::Work);
+    }
+
+    fn change(&mut self, new: State) -> State {
+        self.prev_state = self.state;
+        self.state = new;
+        println!("Changed from {:?} to {:?}", self.prev_state, self.state);
+        self.state
+    }
+}
+
+impl Iterator for Pomodoro {
+    type Item = State;
+
+    fn next(&mut self) -> Option<State> {
+        let next = match self.state {
+            State::Work => State::ShortBreak,
+            State::ShortBreak => State::Work,
+            //State::LongBreak => State::Work,
+            State::Pause => self.prev_state,
+            State::Stop => State::Stop,
+        };
+        match next { State::Stop => None, _ => Some(self.change(next)) } 
     }
 }
