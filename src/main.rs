582a5803883826b3bbecb09161e2a8f343bd7f47
Daniel Olano <danielolanoc@gmail.com>
2018-06-09 11:11:44 +0200

    Add waiting times

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ extern crate docopt;
 extern crate serde_derive;
 
 use docopt::Docopt;
-use std::sync::mpsc;
-use std::thread::{spawn};
+use std::thread::{park_timeout, spawn};
 use std::time::Duration;
 
 const USAGE: &'static str = "
@@ -16,15 +15,15 @@ Options:
   -h --help                 Show this screen.
   -v --version              Show version.
 
-  -l --length=<minutes>     Length of your pomodoro [default: 20]
-  -b --break=<minutes>      Length of your breaks [default: 5]
+  -l --length=<minutes>     Length of your pomodoro [default: 5]
+  -b --break=<minutes>      Length of your breaks [default: 2]
 ";
 
 #[derive(Debug, Deserialize)]
 struct Args {
     arg_task_name: String,
-    flag_length: u16,
-    flag_break: u16,
+    flag_length: u64,
+    flag_break: u64,
 }
 
 fn main() {
@@ -32,25 +31,18 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
 
-    let len = Duration::from_secs((args.flag_length).into());
-    let brk = Duration::from_secs((args.flag_break).into());
-
-    let (tx, rx) = mpsc::channel();
+    let len = Duration::from_secs(args.flag_length);
+    let brk = Duration::from_secs(args.flag_break);
 
     let mut pomodoro = Pomodoro::new(len, brk);
     pomodoro.start();
 
-    spawn(move || {
-        // work a bit
-        tx.send(pomodoro.next());
-        tx.send(pomodoro.next());
-        tx.send(pomodoro.next());
-        tx.send(pomodoro.next());
+    let handle = spawn(move || {
+        for state in pomodoro {
+            println!("Got {:?}", state);
+        }
     });
-
-    for state in rx {
-        println!("Got {:?}", state);
-    }
+    handle.join().unwrap()
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -58,25 +50,25 @@ enum State {
     Stop,
     Pause,
     Work,
-    //LongBreak,
+    LongBreak,
     ShortBreak,
 }
 
 struct Pomodoro {
     dur: Duration,
-    short_brk: Duration,
-    long_brk: Duration,
+    brk: Duration,
     prev_state: State,
     state: State,
 }
+
+const MAX_PAUSE_TIME: u64 = 3_600;
 
 impl Pomodoro {
     fn new(dur: Duration, brk: Duration) -> Pomodoro {
         Pomodoro {
             dur,
-            short_brk: brk,
-            long_brk: brk * 4,
-            prev_state: State::Stop,
+            brk,
+            prev_state: State::Pause,
             state: State::Pause,
         }
     }
@@ -85,11 +77,21 @@ impl Pomodoro {
         self.change(State::Work);
     }
 
-    fn change(&mut self, new: State) -> State {
+    fn change(&mut self, state: State) {
         self.prev_state = self.state;
-        self.state = new;
-        println!("Changed from {:?} to {:?}", self.prev_state, self.state);
-        self.state
+        self.state = state;
+        let dur = match state {
+            State::Work => self.dur,
+            State::ShortBreak => self.brk,
+            State::LongBreak => self.brk * 4,
+            State::Pause => Duration::from_secs(MAX_PAUSE_TIME),
+            State::Stop => Duration::from_secs(0),
+        };
+        println!(
+            "Changed from {:?} to {:?}, wating {:?}",
+            self.prev_state, self.state, dur
+        );
+        park_timeout(dur);
     }
 }
 
@@ -100,10 +102,16 @@ impl Iterator for Pomodoro {
         let next = match self.state {
             State::Work => State::ShortBreak,
             State::ShortBreak => State::Work,
-            //State::LongBreak => State::Work,
+            State::LongBreak => State::Work,
             State::Pause => self.prev_state,
             State::Stop => State::Stop,
         };
-        match next { State::Stop => None, _ => Some(self.change(next)) } 
+        match next {
+            State::Stop => None,
+            _ => {
+                self.change(next);
+                Some(next)
+            }
+        }
     }
 }
